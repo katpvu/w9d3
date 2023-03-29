e8f77f7494916ee9954c6f90d5eef5ca2cca18e8
Ryder_Aguilera <ryderaguilera@hotmail.com>
2023-03-29 15:03:06 -0700

    Saveme

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/application.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/application.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV !== "production") {
   window.API = API;
 }
 
-let followToggleSelector = "";
+let followToggleSelector = ".follow-toggle";
 let infiniteTweetsSelector = "";
 let usersSearchSelector = "";
 let tweetComposeSelector = "";
@@ -18,7 +18,9 @@ let followersSelector = "";
 
 
 document.querySelectorAll(followToggleSelector).forEach((el) => {
+  console.log(el,"el");
   new FollowToggle(el);
+  console.log(el,"el");
 });
 
 document.querySelectorAll(infiniteTweetsSelector).forEach((el) => {
--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
@@ -2,12 +2,18 @@ import { API, broadcast } from "./util";
 
 export default class FollowToggle {
   constructor(toggleButton) {
-    // Your code here
-    
+    this.toggleButton = toggleButton;
+    this.toggleButton.addEventListener('click',this.handleClick.bind(this));
   }
 
   async handleClick(event) {
-    // Your code here
+      event.preventDefault();
+      console.log(this.followState);
+      if(this.followState="unfollowed"){
+        this.follow();
+      }else{
+        this.unfollow();
+      }
   }
 
   async follow() {
@@ -25,10 +31,12 @@ export default class FollowToggle {
   }
 
   get followState() {
+    // this happens when we this.followState
     return this.toggleButton.dataset.followState;
   }
 
   set followState(newState) {
+    // this gets called when we this.followState =
     this.toggleButton.dataset.followState = newState;
     this.render();
   }
